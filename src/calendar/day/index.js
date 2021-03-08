d8869eb224d660c125b92ba2f0bf3865dd01d68a
SKuzmaN <skuzman@gmail.com>
2021-03-08 14:20:28 +0100

    Fix dayProps for custom dayComponent

--- src/calendar/day/index.js
+++ src/calendar/day/index.js
@@ -8,7 +8,6 @@ import {SELECT_DATE_SLOT} from '../../testIDs';
 import BasicDay from './basic';
 import PeriodDay from './period';
 
-
 const basicDayProps = _.omit(BasicDay.propTypes, 'date');
 
 export default class Day extends Component {
@@ -23,7 +22,14 @@ export default class Day extends Component {
   };
 
   shouldComponentUpdate(nextProps) {
-    return shouldUpdate(this.props, nextProps, ['day', 'dayComponent', 'markingType', 'marking', 'onPress', 'onLongPress']);
+    return shouldUpdate(this.props, nextProps, [
+      'day',
+      'dayComponent',
+      'markingType',
+      'marking',
+      'onPress',
+      'onLongPress'
+    ]);
   }
 
   getMarkingLabel() {
@@ -55,7 +61,7 @@ export default class Day extends Component {
     return label;
   }
 
-  getAccessibilityLabel = (day) => {
+  getAccessibilityLabel = day => {
     const {state} = this.props;
     const today = XDate.locales[XDate.defaultLocale].today;
     const isToday = state === 'today'; //TODO: check if 'day' equals 'today' and remove 'state' check
@@ -82,16 +88,16 @@ export default class Day extends Component {
     const {day} = this.props;
     const date = xdateToData(day);
     const Component = this.getDayComponent();
-    const dayProps = extractComponentProps(Component, this.props);
+    const dayProps = extractComponentProps(Day, this.props);
 
     return (
       <Component
-          {...dayProps}
-          date={date}
-          testID={`${SELECT_DATE_SLOT}-${date.dateString}`}
-          accessibilityLabel={this.getAccessibilityLabel(day)}
-        >
-          {date ? day.getDate() : day}
+        {...dayProps}
+        date={date}
+        testID={`${SELECT_DATE_SLOT}-${date.dateString}`}
+        accessibilityLabel={this.getAccessibilityLabel(day)}
+      >
+        {date ? day.getDate() : day}
       </Component>
     );
   }
